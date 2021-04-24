e7fcbcf2bc65474fc1091d9a647832265b090a67
Lim Bang Jin <limbangjin0@gmail.com>
2021-04-24 19:00:15 +0800

    Add cpp template

---
+++ templates/skeleton.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <numeric>
+
+using namespace std;
+
+int main(){
+//remove cin.tie for interactive problems otherwise manual flushing is needed
+    ios::sync_with_stdio(false), cin.tie(0);
+
+    return 0;
+}
+
+//What we know is a drop, what we don't is an ocean.
